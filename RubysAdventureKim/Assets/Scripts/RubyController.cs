bdd7e189e4dcfb09ba598c5453453047d132c7a6
jojo <joannek715967@gmail.com>
2022-11-28 09:56:12 -0800

    rewrote the script for ruby
    made a metal cube prefab and ruby prefab, fixed ruby's pace, have to decorate and add more land.

--- RubysAdventureKim/Assets/Scripts/RubyController.cs
+++ RubysAdventureKim/Assets/Scripts/RubyController.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-    
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Create 2 variables to use the unity built in axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
+    void FixedUpdate()
+    {
         //Create our movement Vector
-        Vector2 position = transform.position;
+        Vector2 position = rigidbody2d.position;
 
         //Create horizontal and vertical movement
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
