9995d5b3bde70c8c0528cefad3252688ed2122d8
jojo <joannek715967@gmail.com>
2022-11-30 09:54:06 -0800

    created rubys health script
    Ruby's health script, updated it as well, filled the scene up more.

--- RubysAdventureKim/Assets/Scripts/RubyController.cs
+++ RubysAdventureKim/Assets/Scripts/RubyController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -12,6 +16,8 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -33,4 +39,11 @@ public class RubyController : MonoBehaviour
 
         rigidbody2d.MovePosition(position);
     }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 1, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 }
+
