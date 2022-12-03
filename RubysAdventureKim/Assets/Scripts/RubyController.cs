98f02943646743b2f08a550007a80cd44ce9ac44
jojo <joannek715967@gmail.com>
2022-12-03 11:33:51 -0800

    Animated Bots
    Created a damage zone, decorated, bots, and now bots animations. Also wrote new scripts and prefabs.

--- RubysAdventureKim/Assets/Scripts/RubyController.cs
+++ RubysAdventureKim/Assets/Scripts/RubyController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float speed = 5.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
     
     public int health { get { return currentHealth; } }
     public int currentHealth;
 
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -31,6 +34,13 @@ public class RubyController : MonoBehaviour
         //Create 2 variables to use the unity built in axes
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -39,14 +49,21 @@ public class RubyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
 
         //Create horizontal and vertical movement
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.0f * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
